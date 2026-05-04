d5e8c12630336473074ee4632517b72ae193d7b7
Musha Khan <muhammedmushakhan@gmail.com>
2026-05-04 19:52:59 +0800

    Few UI tweaks

--- public/index.php
+++ public/index.php
@@ -1,143 +1,123 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>DummyJSON Web App</title>
-        <link rel="stylesheet" href="assets/css/style.css" />
-        <link
-            rel="shortcut icon"
-            href="assets/svg/Logo.svg"
-            type="image/x-icon"
-        />
-    </head>
-    <body>
-        <header>
-            <div class="wrapper">
-                <nav>
-                    <ul>
-                        <li>
-                            <a href="#home-section" class="active">Home</a>
-                        </li>
-                        <li>
-                            <a href="#about-section">About</a>
-                        </li>
-                    </ul>
-                    <div class="logo">DummyJSON</div>
-                    <ul>
-                        <li><a href="login.php">Sign In</a></li>
-                        <li>
-                            <a href="registration.php" class="signup-btn"
-                                >Sign Up</a
-                            >
-                        </li>
-                    </ul>
-                </nav>
-                <section class="home-section" id="home-section">
-                    <div class="hero-section">
-                        <div class="hero-text-section">
-                            <h1>
-                                <span class="title"
-                                    >Develop, Build, and Test.</span
-                                ><br />
-                                Now made easier with the use of Fake Data!
-                            </h1>
-                            <p>
-                                Get Free Fake Rest API for Placeholder JSON Data
-                                for your Frontend.
-                            </p>
-                            <div class="btn">
-                                <a class="getstarted-btn" href="#"
-                                    >Get Started</a
-                                >
-                            </div>
-                        </div>
-                        <div class="hero-img">
-                            <img
-                                class="hero-img-svg"
-                                src="assets/svg/undraw_code-thinking_0vf2.svg"
-                                alt="EEEE"
-                            />
-                        </div>
-                    </div>
-                </section>
-                <hr class="divider" />
-                <section class="about-section" id="about-section">
-                    <div class="hero-section">
-                        <div class="folder-img">
-                            <img
-                                class="folder-img-svg"
-                                src="assets/svg/undraw_folder_8dxv.svg"
-                                alt="EEEE"
-                            />
-                        </div>
-                        <div class="about-text-section">
-                            <h1>
-                                <span class="title"
-                                    >Ready to use Resources.</span
-                                ><br />
-                                From Dummy Text to Placeholder Images —
-                            </h1>
-                            <p>
-                                DummyJSON API allows you to access 10 diverse
-                                datasets with JSON Placeholder Data.
-                            </p>
-                            <div class="btn">
-                                <a class="getstarted-btn" href="#"
-                                    >Try it Now!</a
-                                >
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <hr class="divider" />
-                <footer>
-                    <div class="footer-section">
-                        <hr />
-                        <h1 class="h1-footer">
-                            <span class="title">Support the Developer!</span>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>DummyJSON Web App</title>
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <link
+        rel="shortcut icon"
+        href="assets/svg/Logo.svg"
+        type="image/x-icon" />
+</head>
+
+<body>
+    <header>
+        <div class="wrapper">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#home-section" class="active">Home</a>
+                    </li>
+                    <li>
+                        <a href="#about-section">About</a>
+                    </li>
+                </ul>
+                <div class="logo">DummyJSON</div>
+                <ul>
+                    <li><a href="login.php">Sign In</a></li>
+                    <li>
+                        <a href="registration.php" class="signup-btn">Sign Up</a>
+                    </li>
+                </ul>
+            </nav>
+            <section class="home-section" id="home-section">
+                <div class="hero-section">
+                    <div class="hero-text-section">
+                        <h1>
+                            <span class="title">Develop, Build, and Test.</span><br />
+                            Now made easier with the use of Fake Data!
                         </h1>
                         <p>
-                            DummyJSON API is made by Ovi, show your support by
-                            visiting the <br />
-                            <span class="link"
-                                ><a href="https://dummyjson.com/"
-                                    >Official Website for DummyJSON API</a
-                                ></span
-                            >
+                            Get Free Fake Rest API for Placeholder JSON Data
+                            for your Frontend.
                         </p>
-                        <ul>
-                            <li>
-                                <a href="https://github.com/Ovi/DummyJSON"
-                                    ><img
-                                        src="assets/svg/mdi_github.svg"
-                                        alt="GitHub"
-                                /></a>
-                            </li>
-                            <li>
-                                <a href="https://x.com/DummyJSON"
-                                    ><img
-                                        src="assets/svg/prime_twitter.svg"
-                                        alt="Twitter/X"
-                                /></a>
-                            </li>
-                            <li>
-                                <a
-                                    href="https://www.linkedin.com/company/dummyjson"
-                                    ><img
-                                        src="assets/svg/ri_linkedin-fill.svg"
-                                        alt="LinkedIn"
-                                /></a>
-                            </li>
-                        </ul>
-                        <p class="copyright">
-                            &copy; 2026 DummyJSON API Web App. All Rights
-                            Reserved.
-                        </p>
+                        <div class="btn">
+                            <a class="getstarted-btn" href="#">Get Started</a>
+                        </div>
                     </div>
-                </footer>
-            </div>
-        </header>
-        <script src="/public/assets/js/script.js" defer></script>
-    </body>
+                    <div class="hero-img">
+                        <img
+                            class="hero-img-svg"
+                            src="assets/svg/undraw_code-thinking_0vf2.svg"
+                            alt="EEEE" />
+                    </div>
+                </div>
+            </section>
+            <hr class="divider" />
+            <section class="about-section" id="about-section">
+                <div class="hero-section">
+                    <div class="folder-img">
+                        <img
+                            class="folder-img-svg"
+                            src="assets/svg/undraw_folder_8dxv.svg"
+                            alt="EEEE" />
+                    </div>
+                    <div class="about-text-section">
+                        <h1>
+                            <span class="title">Ready to use Resources.</span><br />
+                            From Dummy Text to Placeholder Images —
+                        </h1>
+                        <p>
+                            DummyJSON API allows you to access 10 diverse
+                            datasets with JSON Placeholder Data.
+                        </p>
+                        <div class="btn">
+                            <a class="getstarted-btn" href="#">Try it Now!</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <hr class="divider" />
+            <footer>
+                <div class="footer-section">
+                    <hr />
+                    <h1 class="h1-footer">
+                        <span class="title">Support the Developer!</span>
+                    </h1>
+                    <p>
+                        DummyJSON API is made by Ovi, show your support by
+                        visiting the <br />
+                        <span class="link"><a href="https://dummyjson.com/">Official Website for DummyJSON API</a></span>
+                    </p>
+                    <ul>
+                        <li>
+                            <a href="https://github.com/Ovi/DummyJSON"><img
+                                    src="assets/svg/mdi_github.svg"
+                                    alt="GitHub" /></a>
+                        </li>
+                        <li>
+                            <a href="https://x.com/DummyJSON"><img
+                                    src="assets/svg/prime_twitter.svg"
+                                    alt="Twitter/X" /></a>
+                        </li>
+                        <li>
+                            <a
+                                href="https://www.linkedin.com/company/dummyjson"><img
+                                    src="assets/svg/ri_linkedin-fill.svg"
+                                    alt="LinkedIn" /></a>
+                        </li>
+                    </ul>
+                    <p class="copyright">
+                        &copy; 2026 DummyJSON API Web App. All Rights
+                        Reserved.
+                    </p>
+                </div>
+            </footer>
+        </div>
+    </header>
+    <script src="assets/js/script.js" defer></script>
+</body>
+
 </html>
